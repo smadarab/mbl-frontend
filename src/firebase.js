3e0ad7e5f8db1a6fa33c573c8ab5c6b9ba04bfb1
Gayathridevi006 <ms.gayathridevi6@gmail.com>
2025-02-01 18:45:58 +0530

    "removed firebase"

--- src/firebase.js
+++ src/firebase.js
@@ -41,18 +41,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA-TKo1bSNl2abjF8ZacOGXTw7vEJmZwTw",
-  authDomain: "baseball-360c5.firebaseapp.com",
-  projectId: "baseball-360c5",
-  storageBucket: "baseball-360c5.firebasestorage.app",
-  messagingSenderId: "726524626669",
-  appId: "1:726524626669:web:3e6719307b5b8068deaee4",
-  measurementId: "G-XGL98X3WSG",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 // Initialize services
 export const auth = getAuth(app);
